3d2fbefcdcb7705764b7a03100be722a562a5cd7
rahul-sijwali <rahulsijwali@gmail.com>
2023-10-13 08:36:59 +0530

    first commit

---
+++ src/main/java/com/rahul/datajpa/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.rahul.datajpa.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rahul.datajpa.model.Tutorial;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+	List<Tutorial> findByPublished(boolean published);
+	List<Tutorial> findByTitleContaining(String title);
+}
